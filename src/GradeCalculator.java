ba2c7791f14b22819e30c73cc50dd5bf9fc9f881
phoenixcryos <143460214+phoenixcryos@users.noreply.github.com>
2023-09-19 23:02:23 -0600

    Update GradeCalculator.java

--- src/GradeCalculator.java
+++ src/GradeCalculator.java
@@ -12,42 +12,42 @@ public class GradeCalculator {
 
         Scanner input = new Scanner(System.in);
 
-        int numberGrade;
+        double numberGrade; 
         String letterGrade = "";
 
-        System.out.println("Enter the number you received to calculate your letter grade:");
+        System.out.println("Please enter your grade:");
 
-        numberGrade = input.nextInt();
+        numberGrade = input.nextDouble(); // Using nextDouble method to get decimal values
 
-
-        if (numberGrade >= 92) {
-            letterGrade = "A";
-        } else if ((numberGrade < 89) > 92) {
-            letterGrade = "A-";
-        } else if (numberGrade < 87 > 89) {
-            letterGrade = "B+";
-        } else if (numberGrade < 82 > 87) {
-            letterGrade = "B";
-        } else if (numberGrade < 79 > 82) {
-            letterGrade = "B-";
-        } else if (numberGrade < 77 > 79) {
-            letterGrade = "C+";
-        } else if (numberGrade < 72 > 77) {
-            letterGrade = "C";
-        } else if (numberGrade < 69 > 72) {
-            letterGrade = "C-";
-        } else if (numberGrade < 67 > 69) {
-            letterGrade = "D+";
-        } else if (numberGrade < 60 > 67) {
-            letterGrade = "D";
-        } else if (numberGrade < 0 > 60) {
-            letterGrade = "F";
-        } else {
-            System.out.println("Incorrect entry. Grade value cannot be less than zero.");
-            boolean b = numberGrade < 0.0;
-            System.out.println("Your calculated letter grade is: " + letterGrade + " - Congratulations!");
-
+        if (numberGrade < 0) {
+            System.out.println("Grade value cannot be less than zero.");
             input.close();
+            return;
+        } else if (numberGrade >= 92) {
+            letterGrade = "A";
+        } else if (numberGrade >= 89) {
+            letterGrade = "A-";
+        } else if (numberGrade >= 87) {
+            letterGrade = "B+";
+        } else if (numberGrade >= 82) {
+            letterGrade = "B";
+        } else if (numberGrade >= 79) {
+            letterGrade = "B-";
+        } else if (numberGrade >= 77) {
+            letterGrade = "C+";
+        } else if (numberGrade >= 72) {
+            letterGrade = "C";
+        } else if (numberGrade >= 69) {
+            letterGrade = "C-";
+        } else if (numberGrade >= 67) {
+            letterGrade = "D+";
+        } else if (numberGrade >= 60) {
+            letterGrade = "D";
+        } else {
+            letterGrade = "F";
         }
+        
+        System.out.println("Your grade is " + letterGrade + ".");
+        input.close();
     }
 }
